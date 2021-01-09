3a7416c4fbc89e58c3d0e351d5a99c135d4b4d79
Loi Ly <vinhloiit1327@gmail.com>
2021-01-10 02:23:46 +0700

    add skip == 0 and set as default

--- tools/extract_frames.py
+++ tools/extract_frames.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument('input_video', type=str, help='Input video path')
     parser.add_argument('output_dir', type=str, help='Output directory where the frames are extracted to')
-    parser.add_argument('--skip', type=int, default=10, help='The number of frames to skip, e.g. 10')
+    parser.add_argument('--skip', type=int, default=0, help='The number of frames to skip, e.g. 10')
     parser.add_argument('--start', '-s', type=int, default=0, help='The start frame index')
     parser.add_argument('--end', '-e', type=int, default=None, help='The end frame index')
     parser.add_argument('--ext', type=str, default='png', help='Image extension')
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     video_path = Path(args.input_video)
-    output_dir = Path(args.output_dir).joinpath(video_path.stem)
+    output_dir = Path(args.output_dir)
     output_dir.mkdir(parents=True, exist_ok=True)
     skip = args.skip
     ext = args.ext
@@ -33,7 +33,7 @@ if __name__ == "__main__":
         if begin_idx <= i:  # exclude the last index since we start from zero
             if end_idx is not None and i >= end_idx:
                 break
-            if i % skip == 0:
+            if skip == 0 or i % skip == 0:
                 output_path = output_dir.joinpath(f'{video_path.stem}_{i}.{ext}')
                 cv2.imwrite(str(output_path), frame)
                 print(f'Wrote to {output_path}')
